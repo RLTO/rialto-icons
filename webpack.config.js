57991c227289828db53c0f5ac262184958b63ecb
Ben Swennen <b_swennen@hotmail.com>
2018-04-06 13:50:57 +0200

    Export icon to lib instead of dist

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: ["./src/Icon.js"],
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "lib"),
     filename: "index.js"
   },
 
