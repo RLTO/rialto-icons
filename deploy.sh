58c5b5ea6d12865befb2d00cb76dea2daae730bf
Ben Swennen <b_swennen@hotmail.com>
2018-12-13 16:00:08 +0100

    Adjust deploy script to fix deployment when README.md updated

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ git fetch
 git remote set-url origin https://${GITHUB_TOKEN}@github.com/RLTO/rialto-icons.git
 
 # Checkout library branch
+git checkout .
 git checkout $BRANCH
 git reset --hard origin/master
 
@@ -25,7 +26,7 @@ yarn build
 
 # Update icon library package
 rm .babelrc .gitignore .travis.yml README.md deploy.sh icon_to_component.py yarn.lock
-rm -rf icons/ 
+rm -rf icons/
 rm -rf svgo/
 rm -rf scripts/
 rm -rf src/
