75b7972fdc40bbf6f0f4faee67be1e3f59e462e1
Antonio <system@rial.to>
2018-12-17 15:45:19 +0000

    Update icons library [ci skip]

---
+++ IncentiveRenewal.js
@@ -0,0 +1,54 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _iconStyle = _interopRequireDefault(require("./iconStyle.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var IncentiveRenewal = function IncentiveRenewal(props) {
+  var dimensions = props.dimensions,
+      onClick = props.onClick,
+      size = props.size,
+      otherProps = _objectWithoutProperties(props, ["dimensions", "onClick", "size"]);
+
+  return _react.default.createElement("svg", _extends({
+    style: _objectSpread({}, (0, _iconStyle.default)(size), dimensions),
+    onClick: onClick
+  }, otherProps), _react.default.createElement("svg", {
+    viewBox: "0 0 18 18",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, _react.default.createElement("path", {
+    d: "M.078 7.81a9.002 9.002 0 0 1 15.766-4.654L18 1v6h-6l2.425-2.425A7.002 7.002 0 0 0 2.07 8H2.06a1 1 0 1 1-2 0H.055a9.02 9.02 0 0 1 .023-.19zm17.844 2.38a9.002 9.002 0 0 1-15.766 4.654L0 17v-6h6l-2.425 2.425A7.002 7.002 0 0 0 15.93 10h.011a1 1 0 1 1 2 0h.005a9.016 9.016 0 0 1-.023.19z",
+    fill: "#152435",
+    "fill-rule": "evenodd"
+  })));
+};
+
+IncentiveRenewal.propTypes = {
+  size: _propTypes.default.string,
+  onClick: _propTypes.default.func,
+  dimensions: _propTypes.default.shape({
+    height: _propTypes.default.string,
+    width: _propTypes.default.string
+  })
+};
+var _default = IncentiveRenewal;
+exports.default = _default;
---
+++ Sms.js
@@ -0,0 +1,53 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _iconStyle = _interopRequireDefault(require("./iconStyle.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var Sms = function Sms(props) {
+  var dimensions = props.dimensions,
+      onClick = props.onClick,
+      size = props.size,
+      otherProps = _objectWithoutProperties(props, ["dimensions", "onClick", "size"]);
+
+  return _react.default.createElement("svg", _extends({
+    style: _objectSpread({}, (0, _iconStyle.default)(size), dimensions),
+    onClick: onClick
+  }, otherProps), _react.default.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 14 13"
+  }, _react.default.createElement("path", {
+    fill: "#91A6B6",
+    d: "M2 0h10a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H7l-3.146 3.146A.5.5 0 0 1 3 11.793V9H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm2.5 3a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3z"
+  })));
+};
+
+Sms.propTypes = {
+  size: _propTypes.default.string,
+  onClick: _propTypes.default.func,
+  dimensions: _propTypes.default.shape({
+    height: _propTypes.default.string,
+    width: _propTypes.default.string
+  })
+};
+var _default = Sms;
+exports.default = _default;
