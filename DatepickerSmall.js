453602e989604d59fa6e335fe87918af17c02b08
Antonio <system@rial.to>
2019-04-02 06:57:36 +0000

    Update icons library [ci skip]

---
+++ DatepickerSmall.js
@@ -0,0 +1,54 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _iconStyle = _interopRequireDefault(require("./iconStyle.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var DatepickerSmall = function DatepickerSmall(props) {
+  var dimensions = props.dimensions,
+      onClick = props.onClick,
+      size = props.size,
+      otherProps = _objectWithoutProperties(props, ["dimensions", "onClick", "size"]);
+
+  return _react.default.createElement("svg", _extends({
+    style: _objectSpread({}, (0, _iconStyle.default)(size), dimensions),
+    onClick: onClick
+  }, otherProps), _react.default.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 10 10"
+  }, _react.default.createElement("path", {
+    fill: "#91A6B6",
+    fillRule: "evenodd",
+    d: "M2.5 2c.276 0 .5-.214.5-.505v-.99A.508.508 0 0 0 2.5 0c-.276 0-.5.214-.5.505v.99c0 .279.232.505.5.505zm5 0c.276 0 .5-.214.5-.505v-.99A.508.508 0 0 0 7.5 0c-.276 0-.5.214-.5.505v.99c0 .279.232.505.5.505zM9 1v.505C9 2.33 8.334 3 7.5 3 6.672 3 6 2.323 6 1.505V1H4v.505C4 2.33 3.334 3 2.5 3 1.672 3 1 2.323 1 1.505V1c-.552 0-1 .446-1 .998v6.005C0 9.106.887 10 2 10h6a2 2 0 0 0 2-1.997V1.998C10 1.447 9.547 1 9 1zm0 6.99C9 8.549 8.547 9 8.003 9H1.997A.998.998 0 0 1 1 7.99V5h8v2.99z"
+  })));
+};
+
+DatepickerSmall.propTypes = {
+  size: _propTypes.default.string,
+  onClick: _propTypes.default.func,
+  dimensions: _propTypes.default.shape({
+    height: _propTypes.default.string,
+    width: _propTypes.default.string
+  })
+};
+var _default = DatepickerSmall;
+exports.default = _default;
---
+++ ViewingWhite.js
@@ -0,0 +1,54 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _iconStyle = _interopRequireDefault(require("./iconStyle.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var ViewingWhite = function ViewingWhite(props) {
+  var dimensions = props.dimensions,
+      onClick = props.onClick,
+      size = props.size,
+      otherProps = _objectWithoutProperties(props, ["dimensions", "onClick", "size"]);
+
+  return _react.default.createElement("svg", _extends({
+    style: _objectSpread({}, (0, _iconStyle.default)(size), dimensions),
+    onClick: onClick
+  }, otherProps), _react.default.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 16 16"
+  }, _react.default.createElement("path", {
+    fill: "#FFF",
+    fillRule: "evenodd",
+    d: "M3.5 4c.276 0 .5-.215.5-.49V.49A.5.5 0 0 0 3.5 0c-.276 0-.5.215-.5.49v3.02a.5.5 0 0 0 .5.49zm2.782 3.75H6.28h.002-.001zM12.5 4a.5.5 0 0 1-.5-.49V.49c0-.275.224-.49.5-.49a.5.5 0 0 1 .5.49v3.02c0 .275-.224.49-.5.49zm-4 6H11v1H8.5v2.5h-1V11H5v-1h2.5V7.5h1V10zM14 2v1.5c0 .828-.666 1.5-1.5 1.5-.828 0-1.5-.666-1.5-1.5V2H5v1.5C5 4.328 4.334 5 3.5 5 2.672 5 2 4.334 2 3.5V2H1c-.552 0-1 .45-1 1.007v11.986C0 15.55.445 16 1 16h14c.552 0 1-.45 1-1.007V3.007C16 2.45 15.556 2 15 2h-1zm1 13H1V6h14v9z"
+  })));
+};
+
+ViewingWhite.propTypes = {
+  size: _propTypes.default.string,
+  onClick: _propTypes.default.func,
+  dimensions: _propTypes.default.shape({
+    height: _propTypes.default.string,
+    width: _propTypes.default.string
+  })
+};
+var _default = ViewingWhite;
+exports.default = _default;
