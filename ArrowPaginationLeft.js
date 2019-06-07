4f1e6299463417ac3e445388a3411a1f06f10d81
Antonio <system@rial.to>
2019-06-07 13:11:34 +0000

    Update icons library [ci skip]

---
+++ ArrowPaginationLeft.js
@@ -0,0 +1,61 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _iconStyle = _interopRequireDefault(require("./iconStyle.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var ArrowPaginationLeft = function ArrowPaginationLeft(props) {
+  var dimensions = props.dimensions,
+      onClick = props.onClick,
+      size = props.size,
+      fill = props.fill,
+      stroke = props.stroke,
+      otherProps = _objectWithoutProperties(props, ["dimensions", "onClick", "size", "fill", "stroke"]);
+
+  return _react.default.createElement("svg", _extends({
+    style: _objectSpread({}, (0, _iconStyle.default)(size), dimensions),
+    onClick: onClick
+  }, otherProps), _react.default.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    viewBox: "0 0 9 8"
+  }, _react.default.createElement("path", {
+    fill: fill,
+    fillRule: "evenodd",
+    d: "M9 .714C9 .32 8.664 0 8.25 0a.77.77 0 0 0-.52.2l-3.5 3.286a.698.698 0 0 0 0 1.031L7.73 7.801A.767.767 0 0 0 8.25 8c.414 0 .75-.32.75-.714a.699.699 0 0 0-.261-.542l-2.93-2.742 2.98-2.792A.695.695 0 0 0 9 .714zm-5 1C4 1.32 3.664 1 3.25 1a.77.77 0 0 0-.52.2L.23 3.486a.698.698 0 0 0 0 1.031L2.73 6.801A.767.767 0 0 0 3.25 7c.414 0 .75-.32.75-.714a.699.699 0 0 0-.261-.542l-1.93-1.742 1.98-1.792A.695.695 0 0 0 4 1.714z"
+  })));
+};
+
+ArrowPaginationLeft.propTypes = {
+  size: _propTypes.default.string,
+  onClick: _propTypes.default.func,
+  dimensions: _propTypes.default.shape({
+    height: _propTypes.default.string,
+    width: _propTypes.default.string
+  }),
+  fill: _propTypes.default.string,
+  stroke: _propTypes.default.string
+};
+ArrowPaginationLeft.defaultProps = {
+  fill: "#DCE6ED"
+};
+var _default = ArrowPaginationLeft;
+exports.default = _default;
